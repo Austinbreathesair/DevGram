2f85f6385ab6de1badda95b7c0748bc2e87988f5
Austinbreathesair <austinbreathesair@gmail.com>
2021-01-13 14:17:02 +0200

    initial commit and project scaffold

---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCr18aPgRpRjiJewfcrZs-wpSwsVdbiUaU",
+  authDomain: "devgram-b263f.firebaseapp.com",
+  projectId: "devgram-b263f",
+  storageBucket: "devgram-b263f.appspot.com",
+  messagingSenderId: "119486377289",
+  appId: "1:119486377289:web:0011a7f2a0b38f14207e98"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+//utils
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+//collection references
+const usersCollection = db.collection('users')
+const postCollection = db.collection('posts')
+const commentsCollection = db.collection('comments')
+const likesCollection = db.collection('likes')
+
+// export utils/refs
+export {
+  db,
+  auth,
+  usersCollection,
+  postCollection,
+  commentsCollection,
+  likesCollection
+}
